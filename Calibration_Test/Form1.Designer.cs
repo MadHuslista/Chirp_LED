40d38acecea5f133052efda3015058fc5f635551
NicoSchiappa <nicolaschiappacasse@gmail.com>
2021-11-29 05:23:56 -0300

    implementación de función de calibración. Falta la búsqueda

--- Calibration_Test/Form1.Designer.cs
+++ Calibration_Test/Form1.Designer.cs
@@ -29,13 +29,240 @@ namespace Calibration_Test
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputChanGroupBox = new System.Windows.Forms.GroupBox();
+            this.inputChannelComboBox = new System.Windows.Forms.ComboBox();
+            this.inputMinValNumeric = new System.Windows.Forms.NumericUpDown();
+            this.inputChanLabel = new System.Windows.Forms.Label();
+            this.inputMaxValLabel = new System.Windows.Forms.Label();
+            this.inputMinValLabel = new System.Windows.Forms.Label();
+            this.inputMaxValNumeric = new System.Windows.Forms.NumericUpDown();
+            this.outputChanGroupBox = new System.Windows.Forms.GroupBox();
+            this.outputChannelComboBox = new System.Windows.Forms.ComboBox();
+            this.outputMinValNumeric = new System.Windows.Forms.NumericUpDown();
+            this.outputMaxValLabel = new System.Windows.Forms.Label();
+            this.outputMinValLabel = new System.Windows.Forms.Label();
+            this.outputMaxValNumeric = new System.Windows.Forms.NumericUpDown();
+            this.outputChanLabel = new System.Windows.Forms.Label();
+            this.calibration_button = new System.Windows.Forms.Button();
+            this.inputChanGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inputMinValNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputMaxValNumeric)).BeginInit();
+            this.outputChanGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.outputMinValNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputMaxValNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputChanGroupBox
+            // 
+            this.inputChanGroupBox.Controls.Add(this.inputChannelComboBox);
+            this.inputChanGroupBox.Controls.Add(this.inputMinValNumeric);
+            this.inputChanGroupBox.Controls.Add(this.inputChanLabel);
+            this.inputChanGroupBox.Controls.Add(this.inputMaxValLabel);
+            this.inputChanGroupBox.Controls.Add(this.inputMinValLabel);
+            this.inputChanGroupBox.Controls.Add(this.inputMaxValNumeric);
+            this.inputChanGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.inputChanGroupBox.Location = new System.Drawing.Point(25, 27);
+            this.inputChanGroupBox.Name = "inputChanGroupBox";
+            this.inputChanGroupBox.Size = new System.Drawing.Size(328, 112);
+            this.inputChanGroupBox.TabIndex = 2;
+            this.inputChanGroupBox.TabStop = false;
+            this.inputChanGroupBox.Text = "Channel Parameters - Input";
+            // 
+            // inputChannelComboBox
+            // 
+            this.inputChannelComboBox.Location = new System.Drawing.Point(152, 24);
+            this.inputChannelComboBox.Name = "inputChannelComboBox";
+            this.inputChannelComboBox.Size = new System.Drawing.Size(168, 21);
+            this.inputChannelComboBox.TabIndex = 1;
+            this.inputChannelComboBox.Text = "Dev1/ai0";
+            // 
+            // inputMinValNumeric
+            // 
+            this.inputMinValNumeric.DecimalPlaces = 2;
+            this.inputMinValNumeric.Location = new System.Drawing.Point(152, 80);
+            this.inputMinValNumeric.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.inputMinValNumeric.Name = "inputMinValNumeric";
+            this.inputMinValNumeric.Size = new System.Drawing.Size(168, 20);
+            this.inputMinValNumeric.TabIndex = 5;
+            this.inputMinValNumeric.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            // 
+            // inputChanLabel
+            // 
+            this.inputChanLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.inputChanLabel.Location = new System.Drawing.Point(16, 26);
+            this.inputChanLabel.Name = "inputChanLabel";
+            this.inputChanLabel.Size = new System.Drawing.Size(96, 16);
+            this.inputChanLabel.TabIndex = 0;
+            this.inputChanLabel.Text = "Input Channel:";
+            // 
+            // inputMaxValLabel
+            // 
+            this.inputMaxValLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.inputMaxValLabel.Location = new System.Drawing.Point(16, 54);
+            this.inputMaxValLabel.Name = "inputMaxValLabel";
+            this.inputMaxValLabel.Size = new System.Drawing.Size(96, 16);
+            this.inputMaxValLabel.TabIndex = 2;
+            this.inputMaxValLabel.Text = "Maximum Value:";
+            // 
+            // inputMinValLabel
+            // 
+            this.inputMinValLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.inputMinValLabel.Location = new System.Drawing.Point(16, 82);
+            this.inputMinValLabel.Name = "inputMinValLabel";
+            this.inputMinValLabel.Size = new System.Drawing.Size(96, 16);
+            this.inputMinValLabel.TabIndex = 4;
+            this.inputMinValLabel.Text = "Minimum Value:";
+            // 
+            // inputMaxValNumeric
+            // 
+            this.inputMaxValNumeric.DecimalPlaces = 2;
+            this.inputMaxValNumeric.Location = new System.Drawing.Point(152, 52);
+            this.inputMaxValNumeric.Name = "inputMaxValNumeric";
+            this.inputMaxValNumeric.Size = new System.Drawing.Size(168, 20);
+            this.inputMaxValNumeric.TabIndex = 3;
+            this.inputMaxValNumeric.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // outputChanGroupBox
+            // 
+            this.outputChanGroupBox.Controls.Add(this.outputChannelComboBox);
+            this.outputChanGroupBox.Controls.Add(this.outputMinValNumeric);
+            this.outputChanGroupBox.Controls.Add(this.outputMaxValLabel);
+            this.outputChanGroupBox.Controls.Add(this.outputMinValLabel);
+            this.outputChanGroupBox.Controls.Add(this.outputMaxValNumeric);
+            this.outputChanGroupBox.Controls.Add(this.outputChanLabel);
+            this.outputChanGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.outputChanGroupBox.Location = new System.Drawing.Point(25, 147);
+            this.outputChanGroupBox.Name = "outputChanGroupBox";
+            this.outputChanGroupBox.Size = new System.Drawing.Size(328, 112);
+            this.outputChanGroupBox.TabIndex = 3;
+            this.outputChanGroupBox.TabStop = false;
+            this.outputChanGroupBox.Text = "Channel Parameters - Output";
+            // 
+            // outputChannelComboBox
+            // 
+            this.outputChannelComboBox.Location = new System.Drawing.Point(152, 24);
+            this.outputChannelComboBox.Name = "outputChannelComboBox";
+            this.outputChannelComboBox.Size = new System.Drawing.Size(168, 21);
+            this.outputChannelComboBox.TabIndex = 1;
+            this.outputChannelComboBox.Text = "Dev1/ao0";
+            // 
+            // outputMinValNumeric
+            // 
+            this.outputMinValNumeric.DecimalPlaces = 2;
+            this.outputMinValNumeric.Location = new System.Drawing.Point(152, 80);
+            this.outputMinValNumeric.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.outputMinValNumeric.Name = "outputMinValNumeric";
+            this.outputMinValNumeric.Size = new System.Drawing.Size(168, 20);
+            this.outputMinValNumeric.TabIndex = 5;
+            this.outputMinValNumeric.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            // 
+            // outputMaxValLabel
+            // 
+            this.outputMaxValLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.outputMaxValLabel.Location = new System.Drawing.Point(16, 54);
+            this.outputMaxValLabel.Name = "outputMaxValLabel";
+            this.outputMaxValLabel.Size = new System.Drawing.Size(96, 16);
+            this.outputMaxValLabel.TabIndex = 2;
+            this.outputMaxValLabel.Text = "Maximum Value:";
+            // 
+            // outputMinValLabel
+            // 
+            this.outputMinValLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.outputMinValLabel.Location = new System.Drawing.Point(16, 82);
+            this.outputMinValLabel.Name = "outputMinValLabel";
+            this.outputMinValLabel.Size = new System.Drawing.Size(96, 16);
+            this.outputMinValLabel.TabIndex = 4;
+            this.outputMinValLabel.Text = "Minimum Value:";
+            // 
+            // outputMaxValNumeric
+            // 
+            this.outputMaxValNumeric.DecimalPlaces = 2;
+            this.outputMaxValNumeric.Location = new System.Drawing.Point(152, 52);
+            this.outputMaxValNumeric.Name = "outputMaxValNumeric";
+            this.outputMaxValNumeric.Size = new System.Drawing.Size(168, 20);
+            this.outputMaxValNumeric.TabIndex = 3;
+            this.outputMaxValNumeric.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // outputChanLabel
+            // 
+            this.outputChanLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.outputChanLabel.Location = new System.Drawing.Point(16, 26);
+            this.outputChanLabel.Name = "outputChanLabel";
+            this.outputChanLabel.Size = new System.Drawing.Size(96, 16);
+            this.outputChanLabel.TabIndex = 0;
+            this.outputChanLabel.Text = "Output Channel:";
+            // 
+            // calibration_button
+            // 
+            this.calibration_button.Location = new System.Drawing.Point(157, 288);
+            this.calibration_button.Name = "calibration_button";
+            this.calibration_button.Size = new System.Drawing.Size(75, 23);
+            this.calibration_button.TabIndex = 4;
+            this.calibration_button.Text = "Calibrar";
+            this.calibration_button.UseVisualStyleBackColor = true;
+            this.calibration_button.Click += new System.EventHandler(this.calibration_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(396, 339);
+            this.Controls.Add(this.calibration_button);
+            this.Controls.Add(this.inputChanGroupBox);
+            this.Controls.Add(this.outputChanGroupBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.inputChanGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.inputMinValNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputMaxValNumeric)).EndInit();
+            this.outputChanGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.outputMinValNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputMaxValNumeric)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox inputChanGroupBox;
+        private System.Windows.Forms.ComboBox inputChannelComboBox;
+        private System.Windows.Forms.NumericUpDown inputMinValNumeric;
+        private System.Windows.Forms.Label inputChanLabel;
+        private System.Windows.Forms.Label inputMaxValLabel;
+        private System.Windows.Forms.Label inputMinValLabel;
+        private System.Windows.Forms.NumericUpDown inputMaxValNumeric;
+        private System.Windows.Forms.GroupBox outputChanGroupBox;
+        private System.Windows.Forms.ComboBox outputChannelComboBox;
+        private System.Windows.Forms.NumericUpDown outputMinValNumeric;
+        private System.Windows.Forms.Label outputMaxValLabel;
+        private System.Windows.Forms.Label outputMinValLabel;
+        private System.Windows.Forms.NumericUpDown outputMaxValNumeric;
+        private System.Windows.Forms.Label outputChanLabel;
+        private System.Windows.Forms.Button calibration_button;
     }
 }
 
--- Calibration_Test/Form1.cs
+++ Calibration_Test/Form1.cs
@@ -1,20 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using NationalInstruments.DAQmx;
+using NationalInstruments;
+using MathNet.Numerics;
 
 namespace Calibration_Test
 {
     public partial class Form1 : Form
     {
+
+        private string CLB_PATH = @"D:\Proyecto_CHIRP_LED\Calibration_Test\logs";
+
+        private List<List<double>> Calibration_Data = new List<List<double>>();
+        private double [][] Calibration_Array = new double [2][] ;
+        private Task inputTask;
+        private Task outputTask;
+        AnalogSingleChannelWriter writer;
+        AnalogSingleChannelReader reader;
+        private AsyncCallback inputCallback;
+
+        private System.Collections.IEnumerator Ref_Enum;
+        private int samples_per_step; 
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            // Initialize UI
+            inputChannelComboBox.Items.AddRange(DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AI, PhysicalChannelAccess.External));
+            outputChannelComboBox.Items.AddRange(DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AO, PhysicalChannelAccess.External));
+
+            if (inputChannelComboBox.Items.Count > 0)   inputChannelComboBox.SelectedIndex = 7; //Cambiar a index 0 antes de sacar a producción
+            if (outputChannelComboBox.Items.Count > 0) outputChannelComboBox.SelectedIndex = 0;
+
+            Calibration_Data.Add(new List<double>());
+            Calibration_Data.Add(new List<double>());
+            
+
+        }
+
+        private void calibration_button_Click(object sender, EventArgs e)
+        {
+            double start_val = 0; //0V
+            double stop_val = 10; // 10V
+            double step = 0.01; //1mV
+
+            double sample_rate = 20000;
+            samples_per_step = 50; // 100;
+
+            double [] Ref_Array = Generate.LinearRange(start_val, step, stop_val);
+            Ref_Enum = Ref_Array.GetEnumerator();
+
+            //Creación de los Task
+            inputTask = new Task();
+            outputTask = new Task();
+
+            //Config de los canales 
+            inputTask.AIChannels.CreateVoltageChannel(
+                inputChannelComboBox.Text,
+                "",
+                AITerminalConfiguration.Rse,
+                Convert.ToDouble(inputMinValNumeric.Value),
+                Convert.ToDouble(inputMaxValNumeric.Value),
+                AIVoltageUnits.Volts
+                );
+
+            inputTask.Timing.ConfigureSampleClock(
+                "",
+                sample_rate,
+                SampleClockActiveEdge.Rising,
+                SampleQuantityMode.ContinuousSamples,
+                samples_per_step
+                );
+
+            outputTask.AOChannels.CreateVoltageChannel(
+                outputChannelComboBox.Text,
+                "",
+                Convert.ToDouble(outputMinValNumeric.Value),
+                Convert.ToDouble(outputMaxValNumeric.Value),
+                AOVoltageUnits.Volts
+                );
+
+            inputTask.Control(TaskAction.Verify);
+            outputTask.Control(TaskAction.Verify);
+
+            writer = new AnalogSingleChannelWriter(outputTask.Stream);
+            reader = new AnalogSingleChannelReader(inputTask.Stream);
+
+            Ref_Enum.MoveNext();
+            writer.WriteSingleSample(true, Convert.ToDouble(Ref_Enum.Current));
+
+
+            inputCallback = new AsyncCallback(InputRead);
+            reader.SynchronizeCallbacks = true;
+            reader.BeginReadMultiSample(samples_per_step, inputCallback, inputTask);
+            calibration_button.Enabled = false;
+
+        }
+
+        private void InputRead(IAsyncResult ar)
+        {
+            double[] data = reader.EndReadMultiSample(ar);
+
+            double data_mean = data.Sum() / data.Length;
+
+            Calibration_Data[0].Add(data_mean);
+            Calibration_Data[1].Add(Convert.ToDouble(Ref_Enum.Current));
+            
+            
+            if (Ref_Enum.MoveNext())
+            {
+                writer.WriteSingleSample(true, Convert.ToDouble(Ref_Enum.Current));
+                reader.BeginReadMultiSample(samples_per_step, inputCallback, inputTask);
+            }
+            else
+            {
+                calibration_button.Enabled = true;
+                Console.WriteLine("Done");
+                Calibration_Array[0] = Calibration_Data[0].ToArray();
+                Calibration_Array[1] = Calibration_Data[1].ToArray();
+                save_calib();
+            }
+
+        }
+
+        private void save_calib()
+        {
+            var calib_sb = new StringBuilder(); 
+            
+            for (int i = 0; i < Calibration_Array[0].Length; i++)
+            {
+                calib_sb.Append(Calibration_Array[0][i].ToString());
+                calib_sb.Append(",");
+                calib_sb.AppendLine(Calibration_Array[1][i].ToString());
+            }
+            File.WriteAllText(CLB_PATH, calib_sb.ToString());
         }
     }
 }
